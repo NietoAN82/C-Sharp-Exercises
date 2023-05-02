138b143698ee8f19812bb638e2a8bbf49586dd36
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-05-02 13:31:34 -0500

    Added Tree Traversal functions

---
+++ TreeTraversal/Traversal.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Node
+    {
+        public int Data;
+        public Node left;
+        public Node right;
+    }
+
+    class Program
+    {
+        static void InOrder(Node root)
+        {
+            if(root != null)
+            {
+                InOrder(root.left);
+                System.Diagnostics.Debug.WriteLine(root.Data);
+                InOrder(root.right);
+            }
+        }
+
+        static void PreOrder(Node root)
+        {
+            if(root != null)
+            {
+                System.Diagnostics.Debug.WriteLine(root.Data);
+                PreOrder(root.left);
+                PreOrder(root.right);
+            }
+        }
+
+        static void PostOrder(Node root)
+        {
+            if(root != null)
+            {
+                PostOrder(root.left);
+                PostOrder(root.right);
+                System.Diagnostics.Debug.WriteLine(root.Data);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Node leftNode = new Node()
+            {
+                Data = 1,
+                left = null,
+                right = null
+            };
+
+            Node rightNode = new Node()
+            {
+                Data = 3,
+                left = null,
+                right = null
+            };
+
+            Node root = new Node()
+            {
+                Data = 2,
+                left = leftNode,
+                right = rightNode
+            };
+
+           PostOrder(root);
+        }
+
+        
+    }
+}
