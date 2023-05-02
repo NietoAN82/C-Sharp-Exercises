1c29fd36ca17202b08ff08b17d3636350701e910
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-05-02 15:40:01 -0500

    Add radixSort

--- SortingExercises/Program.cs
+++ SortingExercises/Program.cs
@@ -362,6 +362,32 @@ namespace SortingExercises
             }
             return arr;
         }
+
+        static int[] radixSort(int[] data)
+        {
+            int[] temp = new int[data.Length];
+
+            for(int shift = 31; shift > -1; shift--)
+            {
+                int j = 0;
+
+                for(int i = 0; i <= data.Length; i++)
+                {
+                    bool move = (data[i] << shift) >= 0;
+
+                    if(shift == 0 ? !move: move)
+                    {
+                        data[i - j] = data[i];
+                    }
+                    else
+                    {
+                        temp[j++] = data[i];
+                    }
+                }
+                Array.Copy(temp, 0, data, data.Length - j, j);
+            }
+            return data;
+        }
         public static void Main(string[] args)
         {
 
