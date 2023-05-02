40b72587b52cd518d18f3103b825f751313f9c43
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-05-02 14:51:26 -0500

    Bubblesort added

--- SortingExercises/Program.cs
+++ SortingExercises/Program.cs
@@ -194,7 +194,23 @@ namespace SortingExercises
 
             return visited;
         }*/
-
+        static int[] bubbleSort(int[] arr)
+        {
+            int len = arr.Length;
+            for(int i = len - 1; i >= 0; i--)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    if (arr[j-1] > arr[j])
+                    {
+                        int temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
         static int[] selectionSort(int[] arr)
         {
             int len = arr.Length;
