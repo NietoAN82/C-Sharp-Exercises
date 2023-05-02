d6a332e07cd0232e73efc78d86c65a9d05c63784
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-05-02 11:34:37 -0500

    Binary Search function added

--- SortingExercises/Program.cs
+++ SortingExercises/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace SortingExercises
+﻿using System.Collections.Generic;
+namespace SortingExercises
 {
     public class Program
     {
@@ -19,6 +20,28 @@
                 }
             }
             return -1;
+        }
+
+        static int binarySearch(List<int> elements, int x)
+        {
+            int start = 0;
+            int stop = elements.Count - 1;
+            int middle = (int)((start + stop) / 2);
+
+            while (elements[middle] != x && start < stop) { 
+                if(x < elements[middle])
+                {
+                    stop = middle - 1;
+                }
+                else
+                {
+                    start = middle + 1;
+                }
+
+                middle = (int)((start + stop) / 2);
+            }
+
+            return (elements[middle] != x) ? -1 : middle; //Return -1 or middle depending if x was found
         }
        
     }
