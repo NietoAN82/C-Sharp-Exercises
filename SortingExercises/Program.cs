0afb9f60e5a075b5377bdff71d3400c94a8acf0a
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-05-02 15:11:40 -0500

    Add Quicksort

--- SortingExercises/Program.cs
+++ SortingExercises/Program.cs
@@ -211,6 +211,48 @@ namespace SortingExercises
             }
             return arr;
         }
+
+        static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right])
+                    {
+                        return right;
+                    }
+                }
+            }
+        }
+
+        static int[] quickSort(int[] arr, int left, int right)
+        {
+            if(left < right)
+            {
+                int pivot = Partition(arr, left, right);
+                if (pivot > 1)
+                {
+                    quickSort(arr, left, pivot - 1);
+                }
+                if(pivot + 1 < right)
+                {
+                    quickSort(arr, pivot + 1, right);
+                }
+            }
+            return arr;
+        }
         static int[] selectionSort(int[] arr)
         {
             int len = arr.Length;
