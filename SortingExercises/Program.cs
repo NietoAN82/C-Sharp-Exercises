a6543b19f92a7bcf798bb6cc5dae1897b5292d44
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-05-02 15:53:20 -0500

    Add shellSort

--- SortingExercises/Program.cs
+++ SortingExercises/Program.cs
@@ -424,7 +424,24 @@ namespace SortingExercises
             }
 
         }
-
+        int[] shellSort(int[] arr)
+        {
+            int length = arr.Length;
+            for(int gap = length / 2; gap > 0; gap /= 2)
+            {
+                for(int i = gap; i < length; i += 1)
+                {
+                    int temp = arr[i];
+                    int j;
+                    for(j = 1; j >= gap && arr[j-gap] > temp; j-= gap)
+                    {
+                        arr[j] = arr[j-gap];
+                    }
+                    arr[j] = temp;
+                }
+            }
+            return arr;
+        }
         static void swap(int[] arr, int element1, int element2)
         {
             int swap = arr[element1];
