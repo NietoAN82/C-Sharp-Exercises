d0d1788900e85518ddbf93e78f132c49b18acd11
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-05-02 15:33:05 -0500

    Add insertionSort

--- SortingExercises/Program.cs
+++ SortingExercises/Program.cs
@@ -349,6 +349,19 @@ namespace SortingExercises
             }
             return arr;
         }
+        static int[] insertionSort(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int value = arr[i];
+                for(int j = i - 1; j > -1 && arr[j] > value; j--)
+                {
+                    arr[j+1] = arr[j];
+                }
+                arr[i + 1] = value;
+            }
+            return arr;
+        }
         public static void Main(string[] args)
         {
 
